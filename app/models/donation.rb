ea6566aa678b87e9d06f0b250c57e586898d4dc9
Nicholas Galante <nicholasgalante94@gmail.com>
2023-08-28 18:39:34 -0400

    Add belongs_to association

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,5 +1,6 @@
 class Donation < ApplicationRecord
-   belongs_to :cause 
+   belongs_to :cause
+   belongs_to :user 
 
    validates :amount, presence: true
    validates :amount, numericality: true

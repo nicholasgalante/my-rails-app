c6c743f2870c9f682024402646b2f548f8a77c36
Nicholas Galante <nicholasgalante94@gmail.com>
2023-08-28 18:21:29 -0400

    Add Destroy method to Cause controller

--- app/controllers/causes_controller.rb
+++ app/controllers/causes_controller.rb
@@ -1,2 +1,34 @@
 class CausesController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
+
+  def index
+    causes = Cause.all
+    render json: causes, status: :ok
+  end
+
+  def show
+    cause = Cause.find(params[:id])
+    render json: cause, status: :ok
+  end
+
+  def create
+    cause = Cause.create!(cause_params)
+    render json: cause, status: :created
+  end
+
+  def destroy
+   cause = Cause.find(params[:id])
+   cause.destroy
+   render json: {}, status: :no_content
+  end
+
+  private
+
+  def cause_params
+    params.permit(:title, :description, :school_name, :city, :state, :image_url)
+  end
+
+  def render_invalid_response(exception)
+      render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
--- app/models/cause.rb
+++ app/models/cause.rb
@@ -2,6 +2,6 @@ class Cause < ApplicationRecord
    has_many :donations
 
    validates :title, :school_name, :city, presence: true
-   validates :description, length: { minimum: 100, message: "must have at least 100 characters" }
-   validates :state, length: { is: 2, "must be 2 characters" }
+   validates :description, length: { minimum: 75, message: "must have at least 75 characters" }
+   validates :state, length: { is: 2, message: "must be 2 characters" }
 end

b9874191cb7c0f68478c54d016afe9ff01ac3822
Nicholas Galante <nicholasgalante94@gmail.com>
2023-08-28 14:20:02 -0400

    Add association and validations to Cause Model

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -1,2 +1,7 @@
 class Cause < ApplicationRecord
+   has_many :donations
+
+   validates :title, :school_name, :city, presence: true
+   validates :description, length: { minimum: 100, message: "must have at least 100 characters" }
+   validates :state, length: { is: 2, "must be 2 characters" }
 end

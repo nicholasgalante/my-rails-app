2c8fa733a093564b0f18ba00b6c607f26d18850c
Nicholas Galante <nicholasgalante94@gmail.com>
2023-08-31 16:24:20 -0400

    Initialize Button style

--- client/src/pages/CauseList.js
+++ client/src/pages/CauseList.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import CauseCard from "../components/CauseCard";
+// import { Link } from "react-router-dom";
+import { Button } from "../styles/Button";
 
 function CauseList() {
   const [causes, setCauses] = useState([]);
@@ -15,9 +17,16 @@ function CauseList() {
 
   return (
     <>
-      {causes.length > 0
-        ? causes.map((cause) => <CauseCard key={cause.id} cause={cause}/>)
-        : "No causes found! Sign up to create one."}
+      {causes.length > 0 ? (
+        causes.map((cause) => <CauseCard key={cause.id} cause={cause} />)
+      ) : (
+        <>
+          <h2>No Causes Found. Sign in to create one!</h2>
+          <Button>
+            Sign In
+          </Button>
+        </>
+      )}
     </>
   );
 }

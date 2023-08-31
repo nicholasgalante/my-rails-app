a7cf7914fe7022619e3761d61cc7f8909587f8e6
Nicholas Galante <nicholasgalante94@gmail.com>
2023-08-31 15:12:14 -0400

    Initialize CauseCard

---
+++ client/src/components/CauseCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function CauseCard({cause}){
+
+   const { title, school_name, city, state, description, image_url } = cause
+   return (
+      <>
+      {title}
+      {school_name}
+      {city}
+      {state}
+      </>
+   )
+
+}
+
+export default CauseCard;

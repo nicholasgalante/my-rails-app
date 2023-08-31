058a0acd81353903fc9a9685bba1c5518843b214
Nicholas Galante <nicholasgalante94@gmail.com>
2023-08-31 13:03:28 -0400

    Delete css files

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,26 @@
-import './App.css';
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+// import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
-  const [user, setUser] = useState(null);
-  
+  // const [user, setUser] = useState(null);
+
   // auto-login
-  useEffect(() => {
-    fetch("/me").then((r) => {
-      if (r.ok) {
-        r.json().then((user) => setUser(user));
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   fetch("/me").then((r) => {
+  //     if (r.ok) {
+  //       r.json().then((user) => setUser(user));
+  //     }
+  //   });
+  // }, []);
 
   return (
     <>
     
+      <main>
+
+      </main>
     </>
-  )
+  );
 }
 
 export default App;

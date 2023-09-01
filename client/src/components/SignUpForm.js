e591049316523adbf3471397fc7bda8400be9f78
Nicholas Galante <nicholasgalante94@gmail.com>
2023-09-01 15:08:51 -0400

    Initialize Textarea component and CSS styling

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Error, Input, FormField, Label, Textarea } from "../styles";
 
 function SignUpForm(){
    return "Sign up form, present!"

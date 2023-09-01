e2e497c98768ea841ddcacbe6f64345dbfb9f23d
Nicholas Galante <nicholasgalante94@gmail.com>
2023-09-01 14:23:49 -0400

    Set form to state

--- client/src/components/SignInForm.js
+++ client/src/components/SignInForm.js
@@ -1,15 +1,41 @@
 import React, { useState } from "react";
-import { FormField, Label, Input } from "../styles";
+import { FormField, Label, Input, Button } from "../styles";
 
 function SignInForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState("");
 
-  return (
-   <form>
+  console.log("Username: ", username);
+  console.log("Password: ", password);
 
-   </form>
+  return (
+    <form>
+      <FormField>
+        <Label>Username</Label>
+        <Input
+          type="text"
+          id="username"
+          autoComplete="off"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </FormField>
+      <FormField>
+        <Label>Password</Label>
+        <Input
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </FormField>
+      <FormField>
+        <Button></Button>
+      </FormField>
+      <FormField></FormField>
+    </form>
   );
 }
 

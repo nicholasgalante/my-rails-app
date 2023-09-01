b1f53d58382ff7244d126cd2eed2134976ebe9b0
Nicholas Galante <nicholasgalante94@gmail.com>
2023-09-01 11:28:57 -0400

    Refactor and DRY up styles imports

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Button, Wrapper, Logo, Nav } from "../styles"
 
-function Navbar(){
-   return
+function Navbar({ user }) {
+  return (
+    <Wrapper>
+      <Logo>App Name</Logo>
+      <Nav>
+         {user ? <Button>Create a Cause</Button> : null }
+         {user ? <Button>Sign out</Button> : <Button>Sign in</Button>}
+      </Nav>
+    </Wrapper>
+  );
 }
 
 export default Navbar;

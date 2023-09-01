857abb8a89025d560b43980782b48b51fb5f173f
Nicholas Galante <nicholasgalante94@gmail.com>
2023-09-01 12:15:49 -0400

    Initialize SignUp and SignIn forms

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { useState } from "react";
+import SignInForm from "../components/SignInForm";
+import SignUpForm from "../components/SignUpForm";
 
 function SignIn() {
-  return <>Sign in here!</>;
+  const [showSignIn, setShowSignIn] = useState(true);
+
+  return <>{showSignIn ? <><SignInForm/>          
+  <p>
+  Don't have an account? &nbsp;
+  <Button color="secondary" onClick={() => setShowLogin(false)}>
+    Sign Up
+  </Button>
+</p>
+</> : <><SignUpForm/></>}</>;
 }
 
 export default SignIn;

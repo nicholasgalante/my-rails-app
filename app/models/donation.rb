23b75a9805cde6a5b686151001abd00d594af257
Nicholas Galante <nicholasgalante94@gmail.com>
2023-09-05 12:42:21 -0400

    Update validations for amount and goal attributes

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -3,5 +3,5 @@ class Donation < ApplicationRecord
    belongs_to :user 
 
    validates :amount, presence: true
-   validates :amount, numericality: true
+   validates :amount, presence: true, format: { with: /\A\d+(\.\d{1,2})?\z/, message: "should be in dollars and cents format (e.g., 25.00)" }
 end

ecbf05d5d3090bea63ec11b3889c823da31017ff
Nicholas Galante <nicholasgalante94@gmail.com>
2023-09-30 13:58:48 -0400

    Add hover to signout button

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, NavWrapper, Logo, Nav } from "../styles";
 import { Link } from "react-router-dom";
 
 function Navbar({ user, handleSignOut }) {
+  const [buttonText, setButtonText] = useState("");
+  const [hovered, setHovered] = useState(false);
+
+  useEffect(() => {
+    if (user && user.username) {
+      setButtonText(user.username);
+    } 
+  }, [user]);
+
+  const handleHover = () => {
+    setHovered(!hovered);
+    setButtonText("Sign Out");
+  };
+
+  const handleUnHover = () => {
+    setHovered(false);
+    setButtonText(user.username);
+  };
+
   return (
     <NavWrapper>
-      <Link to="/">
+      <Link to="/causes">
         <Logo>ClassAid</Logo>
       </Link>
       <Nav>
@@ -14,9 +33,16 @@ function Navbar({ user, handleSignOut }) {
             <Button>Create a Cause</Button>
           </Link>
         ) : null}
-        {user ? <Button>{user.username}</Button> : null}
         {user ? (
-          <Link to="/"><Button onClick={handleSignOut}>Sign out</Button></Link>
+          <Link to="/causes">
+            <Button
+              onClick={handleSignOut}
+              onMouseEnter={handleHover}
+              onMouseLeave={handleUnHover}
+            >
+              {buttonText}
+            </Button>
+          </Link>
         ) : (
           <Link to="/signin">
             <Button>Sign in</Button>
@@ -28,3 +54,24 @@ function Navbar({ user, handleSignOut }) {
 }
 
 export default Navbar;
+
+// import React, { useState } from 'react';
+// import './Button.css'; // Import your CSS file for styling
+
+// const MyButton = () => {
+//   const [buttonText, setButtonText] = useState('Default Text');
+//   const [hovered, setHovered] = useState(false);
+
+//   const handleHover = () => {
+//     setHovered(!hovered);
+//     setButtonText(hovered ? 'Default Text' : 'New Text On Hover');
+//   };
+
+//   return (
+//     <button className="custom-button" onMouseEnter={handleHover} onMouseLeave={handleHover}>
+//       {buttonText}
+//     </button>
+//   );
+// };
+
+// export default MyButton;

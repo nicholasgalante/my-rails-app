e2c6b5c06f5a7e70bb68a5716fac7a24d53a7726
Nicholas Galante <nicholasgalante94@gmail.com>
2023-10-09 14:34:22 -0400

    Update cause validations

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -4,6 +4,6 @@ class Cause < ApplicationRecord
 
    validates :title, :school_name, :city, :goal, presence: true
    validates :goal, presence: true, format: { with: /\A\d+(\.\d{1,2})?\z/, message: "should be in dollars and cents format (e.g., 25.00)" }
-   # validates :description, length: { minimum: 75, message: "must have at least 75 characters" }
+   validates :description, length: { minimum: 75, message: "must have at least 75 characters" }
    validates :state, length: { is: 2, message: "must be 2 characters" }
 end

595455820c28ab28320307becef20c63c0d3a950
Nicholas Galante <nicholasgalante94@gmail.com>
2023-10-17 18:01:03 -0400

    Delete myCauses route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :causes
-  get "/mycauses", to: "causes#my_causes"
   resources :donations
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
